4a0b87ed02643684bf83afa5a5a33aa166b16e5b
silajav <silajawed@gmail.com>
2026-05-11 06:14:59 -0700

    Create recruiter_navbar.php

---
+++ recruiter_navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="recruiter_dashboard.php">Job Portal – Recruiter</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#recNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="recNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="recruiter_post_job.php">Post Job</a></li>
+                <li class="nav-item"><a class="nav-link" href="recruiter_jobs.php">My Jobs</a></li>
+                <li class="nav-item"><a class="nav-link" href="recruiter_company.php">Company Profile</a></li>
+                <li class="nav-item"><a class="nav-link" href="recruiter_logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
